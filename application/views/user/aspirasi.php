7fac60023ba471a966c2681a947839b1c722b464
muhammad <saifulmuhammad414@gmail.com>
2019-10-11 09:30:02 +0700

    Fixed aspirasi page

--- application/views/user/aspirasi.php
+++ application/views/user/aspirasi.php
@@ -35,44 +35,35 @@
                 <div class="card card-data_diri">
                     <div class="card-body">
                         <form action="<?= site_url()?>/devote/input_aspirasi" method="post" class="form">
-                            <div class="form-group">
-<<<<<<< HEAD
-                                <textarea name="isi" id="" class="form-control" placeholder="Aspirasi..."></textarea>
-                                <!-- <a href="<?= site_url()?>/devote/keluar" class="btn btn-kirim">Kirim</a> -->
-                            </div>
-                            <div class="button">
-                                <a href="<?= site_url()?>/devote/login/logout" class="btn btn-lanjut">Lewati</a>
-                                <button type="submit" class="btn btn-kirim">Kirim</button>
-=======
                             <h5 class="text-center">Apa harapan kamu untuk OSIS periode 2019/2020?</h5 class="text-center">
-                                <textarea name="aspirasi" id="aspirasi" class="form-control" placeholder="tulis harapan kamu disini..."></textarea>
+                                <textarea name="isi" id="aspirasi" class="form-control" placeholder="tulis harapan kamu disini..."></textarea>
                             </div>
                             <div class="row">
                                 <div class="col-12 col-sm-12">
-                                    <button class="btn-kirim">SIMPAN</button>
+                                    <button class="btn-kirim" type="submit">SIMPAN</button>
                                 </div>
->>>>>>> 724a1f45d134a104d7db78582f6be00a03b1b04e
                             </div>
                         </form>
                     </div>
-                </div>
+                
                 <p class="text-center mt-5">Beberapa harapan dari teman-teman</p>
+            </div>
             </div>
             <div class="container">
             <div class="row">
                 <div class="col-12 col-sm-12">
                     <div class="owl-carousel">
-                        <?php for ($i=0; $i < 10; $i++):?>
+                        <?php foreach ($aspirasi as $aspirasi ) { ?>
                         <div class="harapan">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <p><?= $aspirasi['isi']?></p>
                             <div class="photo">
                                 <img src="<?= base_url() ?>public/img/deni.JPG" alt="photo-profile">
                                 <center>
-                                    <span class="text-center">Deni Juli</span>
+                                    <span class="text-center"><?= $aspirasi['nama']?></span>
                                 </center>
                             </div> 
                         </div>
-                        <?php endfor; ?>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
